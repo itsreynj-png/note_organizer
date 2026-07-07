b668fb28f280a042fcf14363d7bff7ed1dacf243
itsreynj-png <itsreynj@gmail.com>
2026-07-07 22:28:16 +0330

    first commit

--- note_management/notes/views.py
+++ note_management/notes/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth import UserCreationForm,AuthenticationForm
 
-# Create your views here.
+def register(request):
+    if request.method=="POST":
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)    
+    else:
+        form =UserCreationForm()
+    return render(request,"register.html",{"form":form})
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+    return render(request, "login.html")

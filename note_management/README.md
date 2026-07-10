2e4a251848c29619c10c72916f97857a6ba17e0f
itsreynj-png <itsreynj@gmail.com>
2026-07-10 18:19:32 +0330

    test

---
+++ note_management/README.md
@@ -0,0 +1,82 @@
+# Note Organizer
+
+## Project Description
+
+Note Organizer is a Django-based web application that helps students organize their academic notes.
+
+Users can:
+- Register and Login
+- Manage Courses
+- Create, Edit and Delete Notes
+- Search Notes by Title or Content
+
+---
+
+## Technologies Used
+
+- Python 3.13
+- Django
+- SQLite
+- Docker
+- Git & GitHub
+
+---
+
+## Installation
+
+1. Clone the repository
+
+```bash
+git clone <repository-url>
+```
+
+2. Enter the project folder
+
+```bash
+cd note_management
+```
+
+3. Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+4. Apply migrations
+
+```bash
+python manage.py migrate
+```
+
+5. Run the server
+
+```bash
+python manage.py runserver
+```
+
+---
+
+## Docker
+
+Build and run the project:
+
+```bash
+docker compose up --build
+```
+
+---
+
+## Team Members
+
+- Reihaneh Najafi
+- Paria Rabani
+
+---
+
+## Features
+
+- User Authentication
+- Course Management
+- Note Management
+- Note Search
+- Docker Support
--- note_management/notes/tests.py
+++ note_management/notes/tests.py
@@ -3,6 +3,109 @@ from django.contrib.auth.models import User
 from .models import Course,Note
 
 
+class UserTest(TestCase):
+     
+    def test_create_user(self):
+        user = User.objects.create_user(
+                username="old user",
+                password="123456"
+          )
+        self.assertEqual(user.username,"old user")
+
+
+    def test_login(self):
+        User.objects.create_user(
+                username="old user",
+                password="123456"
+        )
+
+        login =self.client.login(
+                username="old user",
+                password="123456"
+        )
+        
+        self.assertTrue(login)
+
+    def test_course_belongs_to_user(self):
+        user = User.objects.create_user(
+                username="old user",
+                password="123456"
+        )
+
+        course =Course.objects.create(
+            title="Python",
+            user=user
+        )
+
+        self.assertEqual(course.user,user)
+
+    def test_user_only_see_his_courses(self):
+        user1=User.objects.create(
+            username="new user",
+            password="654321"
+        )
+
+        user2=User.objects.create(
+            username="old user",
+            password="123456"
+        )
+
+        Course.objects.create(
+            title="Math",
+            user=user1
+        )
+
+        Course.objects.create(
+            title="Python",
+            user=user2
+        )
+
+        self.assertTrue(Course.objects.filter(user=user1,title="Math").exists())
+
+
+class courseModelTest(TestCase):
+    def setUp(self):
+        self.user=User.objects.create_user(
+                username="old user",
+                password="123456"
+          )
+    def test_create_course(self):
+        course=Course.objects.create(
+            title="Python",
+            description="python course",
+            user=self.user
+        )
+
+        self.assertEqual(course.title,"Python")
+        self.assertEqual(course.description,"python course")
+        self.assertEqual(course.user,self.user)
+
+
+class NoteModelTest(TestCase):
+    def setUp(self):
+        self.user=User.objects.create_user(
+                username="old user",
+                password="123456"
+          )
+
+        self.course=Course.objects.create(
+            title="Python",
+            description="python course",
+            user=self.user
+        )
+    def test_create_note(self):
+
+        note = Note.objects.create(
+            title="note_management",
+            content="This project helps us manage our course notes",
+            course=self.course
+        )
+
+        self.assertEqual(note.title,"note_management")
+        self.assertEqual(note.content,"This project helps us manage our course notes")
+        self.assertEqual(note.course,self.course)
+
+
 class NoteSearchTest(TestCase):
 
     def setUp(self):
@@ -13,6 +116,7 @@ class NoteSearchTest(TestCase):
 
         self.course=Course.objects.create(
             title="Python",
+            description="python course",
             user=self.user
         )
 
